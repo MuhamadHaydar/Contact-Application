9912c86f44a1b0a6d78d14f70b63bb7354c72db6
MuhamadHaydar <49116664+MuhamadHaydar@users.noreply.github.com>
2020-05-14 13:45:25 +0300

    Create ContactManager class and add some methods

---
+++ src/ContactManager.java
@@ -0,0 +1,33 @@
+// Manage Contacts from this class.
+public class ContactManager {
+
+    // Array of Contacts to store contact.
+    Contact [] myContact;
+    // number of current contact;
+    int currentContact;
+
+    // Default Constructor.
+    ContactManager(){
+        // Current num of Contract.
+        currentContact = 0;
+        // Having 500 slots.
+        myContact = new Contact[500];
+    }
+
+    // Add contact to myContact Array.
+    private void addContact(Contact newContact){
+        myContact[currentContact] = newContact;
+        currentContact ++;
+    }
+
+    // Search by name of contact.
+    private Contact searchContact(String nameContact){
+        // Search over whole of contacts.
+        for(int i = 0; i < currentContact; i++){
+            if(myContact[i].name.equals(nameContact)){
+                return myContact[i];
+            }
+        }
+        return null;
+    }
+}
